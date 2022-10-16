eb17f428898b6691272129aa102e0e2946c229d6
John Vinh <john.michael.vinh2@gmail.com>
2022-10-15 20:33:46 -0400

    Test class now takes file inputs

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,25 @@
+import aoc2021day1.Part1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
 public class Test {
     public static void main(String[] args) {
-        
+        ArrayList<String> lines = new ArrayList<>();
+
+        try (FileReader fr = new FileReader("input.txt"); BufferedReader in = new BufferedReader(fr)) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+
+        }
+
+        Part1 solution = new Part1(lines);
+        System.out.println(solution.solve());
     }
 }
