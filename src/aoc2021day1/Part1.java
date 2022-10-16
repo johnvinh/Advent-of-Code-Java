f95ac91f7af9ecdcf789ae7f8f6ccb0e1f099197
John Vinh <john.michael.vinh2@gmail.com>
2022-10-15 20:39:02 -0400

    Solved 2021 day 1 part 1

--- src/aoc2021day1/Part1.java
+++ src/aoc2021day1/Part1.java
@@ -1,4 +1,22 @@
 package aoc2021day1;
 
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
 public class Part1 {
+    private final ArrayList<String> lines;
+
+    public Part1(ArrayList<String> lines) {
+        this.lines = lines;
+    }
+
+    public int solve() {
+        int numIncreases = 0;
+        for (int i = 1; i < lines.size(); i++) {
+            if (Integer.parseInt(lines.get(i)) > Integer.parseInt(lines.get(i - 1))) {
+                numIncreases++;
+            }
+        }
+        return numIncreases;
+    }
 }
