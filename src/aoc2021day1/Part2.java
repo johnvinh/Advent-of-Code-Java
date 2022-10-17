06cf467e2ef5868da771d5d6f6cbbfc60c51f3a5
John Vinh <john.michael.vinh2@gmail.com>
2022-10-17 11:55:46 -0400

    Added final and private properties to input member

--- src/aoc2021day1/Part2.java
+++ src/aoc2021day1/Part2.java
@@ -3,7 +3,7 @@ package aoc2021day1;
 import java.util.ArrayList;
 
 public class Part2 {
-    ArrayList<String> input;
+    private final ArrayList<String> input;
 
     Part2(ArrayList<String> input) {
         this.input = input;
