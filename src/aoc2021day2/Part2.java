2ed95dab755c1fb06220077ff2ae01b5134302c1
John Vinh <john.michael.vinh2@gmail.com>
2022-10-18 10:32:24 -0400

    Solved 2021 day 2 part 2

--- src/aoc2021day2/Part2.java
+++ src/aoc2021day2/Part2.java
@@ -1,4 +1,31 @@
 package aoc2021day2;
 
+import java.util.ArrayList;
+
 public class Part2 {
+    ArrayList<String> input;
+
+    public Part2(ArrayList<String> input) {
+        this.input = input;
+    }
+
+    public int solve() {
+        int horizontalPositon = 0;
+        int depth = 0;
+        int aim = 0;
+        for (int i = 0; i < input.size(); i++) {
+            String[] operationMagnitude = input.get(i).split(" ", 2);
+            String operation = operationMagnitude[0];
+            int magnitude = Integer.parseInt(operationMagnitude[1]);
+            if (operation.equals("forward")) {
+                horizontalPositon += magnitude;
+                depth += aim * magnitude;
+            } else if (operation.equals("down")) {
+                aim += magnitude;
+            } else if (operation.equals("up")) {
+                aim -= magnitude;
+            }
+        }
+        return horizontalPositon * depth;
+    }
 }
