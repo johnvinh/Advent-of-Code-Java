a2aff8b07220afc384f4f0ccd2d6a8ed1c4cc1f6
John Vinh <john.michael.vinh2@gmail.com>
2022-10-22 16:31:58 -0400

    Create 2021 day 3 part 2 constructor

--- src/aoc2021day3/Part2.java
+++ src/aoc2021day3/Part2.java
@@ -1,4 +1,11 @@
 package aoc2021day3;
 
+import java.util.ArrayList;
+
 public class Part2 {
+    ArrayList<String> input;
+
+    Part2(ArrayList<String> input) {
+        this.input = input;
+    }
 }
