92836fc6a0fc47d54340b49edea9cfe64065b975
John Vinh <john.michael.vinh2@gmail.com>
2022-10-23 09:48:12 -0400

    Replace 2020 magic number with a constant

--- src/aoc2020day1/Part1.java
+++ src/aoc2020day1/Part1.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Part1 {
     ArrayList<String> input;
+    private final int TARGET_VALUE = 2020;
 
     public Part1(ArrayList<String> input) {
         this.input = input;
@@ -20,7 +21,7 @@ public class Part1 {
         int num2 = 0;
         for (int i = 0; i < input.size(); i++) {
             for (int j = i + 1; j < input.size(); j++) {
-                if (numbers.get(i) + numbers.get(j) == 2020) {
+                if (numbers.get(i) + numbers.get(j) == TARGET_VALUE) {
                     num1 = numbers.get(i);
                     num2 = numbers.get(j);
                 }
