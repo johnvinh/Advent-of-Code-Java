78d64ee81523c2ae7fd57353b60dd78617d6c809
John Vinh <john.michael.vinh2@gmail.com>
2022-10-24 12:17:18 -0400

    Solve 2020 day 1 part 2

--- src/Test.java
+++ src/Test.java
@@ -19,7 +19,7 @@ public class Test {
             System.exit(1);
         }
 
-        aoc2020day1.Part1 solution = new aoc2020day1.Part1(lines);
+        aoc2020day1.Part2 solution = new aoc2020day1.Part2(lines);
         System.out.println(solution.solve());
     }
 }
--- src/aoc2020day1/Part2.java
+++ src/aoc2020day1/Part2.java
@@ -8,4 +8,22 @@ public class Part2 {
     public Part2(ArrayList<String> input) {
         this.input = input;
     }
+
+    public int solve() {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        for (int i = 0; i < input.size(); i++) {
+            numbers.add(Integer.parseInt(input.get(i)));
+        }
+
+        for (int i = 0; i < numbers.size(); i++) {
+            for (int j = i + 1; j < numbers.size(); j++) {
+                for (int k = j + 1; k < numbers.size(); k++) {
+                    if (numbers.get(i) + numbers.get(j) + numbers.get(k) == 2020) {
+                        return numbers.get(i) * numbers.get(j) * numbers.get(k);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
 }
