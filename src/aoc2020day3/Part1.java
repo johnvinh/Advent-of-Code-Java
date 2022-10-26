32232d65591303d225d20207e8122e7359df7f0a
John Vinh <john.michael.vinh2@gmail.com>
2022-10-26 17:08:05 -0400

    Create constructor

--- src/aoc2020day3/Part1.java
+++ src/aoc2020day3/Part1.java
@@ -1,4 +1,11 @@
 package aoc2020day3;
 
+import java.util.ArrayList;
+
 public class Part1 {
+    ArrayList<String> input;
+
+    Part1(ArrayList<String> input) {
+        this.input = input;
+    }
 }
