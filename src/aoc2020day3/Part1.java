3591fbb5b7db3e03cfcfbcda3aa9cbd09daf970a
John Vinh <john.michael.vinh2@gmail.com>
2022-10-29 10:37:07 -0400

    Make progress on 2020 day 3 part 1

--- src/aoc2020day3/Part1.java
+++ src/aoc2020day3/Part1.java
@@ -5,11 +5,30 @@ import java.util.ArrayList;
 public class Part1 {
     ArrayList<String> input;
 
-    Part1(ArrayList<String> input) {
+    public Part1(ArrayList<String> input) {
         this.input = input;
     }
 
     public int solve() {
-        return 0;
+        final int LINE_LENGTH = 11;
+        int numTrees = 0;
+        int currentLinePosition = 0;
+        for (String line : input) {
+            int spacesTravelledRight = 0;
+            if (currentLinePosition == LINE_LENGTH) {
+                currentLinePosition = 0;
+            }
+            if (line.charAt(currentLinePosition) == '#') {
+                numTrees++;
+            }
+            while (true) {
+                currentLinePosition++;
+                spacesTravelledRight++;
+                if (spacesTravelledRight == 3) {
+                    break;
+                }
+            }
+        }
+        return numTrees;
     }
 }
