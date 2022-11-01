60b726cea938451aceb4b8e2ee5a8838091f0cab
John Vinh <john.michael.vinh2@gmail.com>
2022-11-01 15:59:00 -0400

    Create 2019 day 2 part 2 constructor

--- src/aoc2019day2/Part2.java
+++ src/aoc2019day2/Part2.java
@@ -1,4 +1,11 @@
 package aoc2019day2;
 
+import java.util.ArrayList;
+
 public class Part2 {
+    ArrayList<String> input;
+
+    Part2(ArrayList<String> input) {
+        this.input = input;
+    }
 }
