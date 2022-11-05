d44f24388bf90b3e56b6e226e116cafe857d0e7e
John Vinh <john.michael.vinh2@gmail.com>
2022-11-05 11:24:13 -0400

    Create 2016 day 1 part 1 constructor

--- src/aoc2016/day1/Part1.java
+++ src/aoc2016/day1/Part1.java
@@ -1,4 +1,11 @@
 package aoc2016.day1;
 
+import java.util.ArrayList;
+
 public class Part1 {
+    ArrayList<String> input;
+
+    Part1(ArrayList<String> input) {
+        this.input = input;
+    }
 }
